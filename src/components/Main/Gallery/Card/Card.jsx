48a901b2c3b62df36ec23930da4a4c23e82854b6
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-01 17:03:21 +0200

    Changement de structure pour la carte

---
+++ src/components/Main/Gallery/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = ({ data }) => {
+  return (
+    <div className="card">
+        <div>
+            <img src={data.cover} alt={data.title} />
+        </div>
+        <div className="card-title">
+        <p>{data.title}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
