09851b8ab64be69c53c782ec041e0e86b0d7b8ca
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-01 17:49:52 +0200

    Changement de l'adresse pour le routage

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home/Index';
+import Home from './pages/Home';
 
 
 const Routes = () => {
