b895826f7be9a80458e25fd9c1b69e596abefb21
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-04 18:01:18 +0200

    Ajout d'un reload pour refesh la page au clique

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -5,26 +5,34 @@ const Nav = () => {
   const location = useLocation();
 
   return (
-    <nav className="nav">
-      <ul className="nav-item">
-        <li>
-          <Link
-            to="/"
-            className={location.pathname === "/" ? "nav-link active" : "nav-link"}
-          >
-            Accueil
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/about"
-            className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
-          >
-            A propos
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div id="global">
+      <nav className="nav">
+        <ul className="nav-item">
+          <li>
+            <Link
+              to="/"
+              className={location.pathname === "/" ? "nav-link active" : "nav-link"}
+              onClick={(e) => {
+                if (location.pathname === "/") {
+                  e.preventDefault();
+                  window.location.reload();
+                }
+              }}
+            >
+              Accueil
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
+            >
+              A propos
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
