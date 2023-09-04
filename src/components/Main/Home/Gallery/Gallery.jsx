240146c342e845b04bab32ae9f85f442ea788cf3
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-04 18:07:32 +0200

    Déplacement du fichier

---
+++ src/components/Main/Home/Gallery/Gallery.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import data from "../../../../data/data.json";
+import Card from "./Card/Card";
+
+const Gallery = () => {
+  return (
+    <div className="gallery">
+      {data.map((item) => (
+        <Card key={item.id} data={item} />
+      ))}
+    </div>
+  );
+};
+
+export default Gallery;
