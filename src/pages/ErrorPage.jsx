282a40785f39fd1cd72f9dceb0688a700b6ded02
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-04 18:08:06 +0200

    Structure fichier ErrorPage

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import Error from "../components/Error/Error404";
+import Footer from "../components/Footer/Footer";
+
+function ErrorPage() {
+  return (
+    <div id="global">
+      <Header />
+      <Error />
+      <Footer />
+    </div>
+  );
+}
+
+export default ErrorPage;
