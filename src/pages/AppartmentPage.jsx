cfdadc0eba5e4d40131709f485f8992f6fc50ab2
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-04 18:07:51 +0200

    Code structure pour AppartmentPage

---
+++ src/pages/AppartmentPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../Header/Header";
+import Main from "./Components/Main";
+import Footer from "../../Footer/Footer";
+
+const AppartmentPage = () => {
+    return (
+        <div id="global">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
+};
+
+export default AppartmentPage;
