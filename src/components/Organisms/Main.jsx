59775a5b77f5067d1f98da3982c51106c55732d0
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-07 17:28:01 +0200

    Changement structure total pour affichage

--- src/components/Organisms/Main.jsx
+++ src/components/Organisms/Main.jsx
@@ -1,19 +1,65 @@
-import React from "react";
-import Banner from "../Molecules/Banner";
-import Gallery from "./Gallery";
-import AboutBlock from "../Organisms/AboutBlock";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import AboutBlock from './AboutBlock';
+import Banner from '../Molecules/Banner';
+import Gallery from './Gallery';
+import aboutData from '../../data/about.json';
 
-const Main = ({ page }) => {
-    return (
-        <main className="main">
-            <Banner page={page} />
-            {page === "AboutPage" ? (
-                <AboutBlock />
-            ) : (
-                <Gallery />
-            )}
-        </main>
-    );
+const AboutPageMain = () => {
+  return (
+    <main className="main">
+      <Banner page="AboutPage"/>
+      <AboutBlock items={aboutData} />
+    </main>
+  );
+};
+
+const ErrorPageMain = () => {
+  return (
+    <main className="main">
+      {/* Contenu pour la page d'erreur */}
+    </main>
+  );
+};
+
+const AppartmentPageMain = () => {
+  return (
+    <main className="main">
+      {/* Contenu pour la page d'appartement */}
+    </main>
+  );
+};
+
+const HomePageMain = () => {
+  return (
+    <main className="main">
+      <Banner />
+      <Gallery />
+    </main>
+  );
+};
+
+const Main = () => {
+  const location = useLocation();
+
+  const getPageContent = (() => {
+    switch (location.pathname) {
+      case '/about':
+        return <AboutPageMain />;
+      case '/error404':
+        return <ErrorPageMain />;
+      case '/appartment':
+        return <AppartmentPageMain />;
+      default:
+        return <HomePageMain />;
+    }
+  })();
+
+  return (
+    <main className="main">
+        {getPageContent}
+    </main>
+  );
 };
 
 export default Main;
