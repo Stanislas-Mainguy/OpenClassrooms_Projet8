452f691cc45fee3d038377f726d18f9428f869fa
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-07 17:26:39 +0200

    changement adresse arrow

--- src/components/Atoms/CollapseArrow.jsx
+++ src/components/Atoms/CollapseArrow.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import ArrowImage from '../../assets/pictures/arrow-about.svg';
 
 const CollapseArrow = () => {
-  return <img src="../../assets/pictures/arrow-about.svg" alt="Arrow" />;
+  return <img src={ArrowImage} alt="Arrow" />;
 };
 
 export default CollapseArrow;
