2d4ee23be58f576a815aba58478729b0097a03c0
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-08 18:16:37 +0200

    Changement de structure poufaciliter l'intégration

--- src/components/Atoms/CollapseDescription.jsx
+++ src/components/Atoms/CollapseDescription.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import aboutData from '../../data/about.json';
 
-const CollapseDescription = () => {
-  const description = aboutData.description;
-  return <p>{description}</p>;
+const CollapseDescription = ({ description }) => {
+  return (
+    <>
+      <p>{description}</p>
+    </>
+  );
 };
 
 export default CollapseDescription;
