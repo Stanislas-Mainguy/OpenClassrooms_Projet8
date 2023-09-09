b1d8d0f35af7861b47a2eb33f04a4e5553eb86a8
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-09 02:48:53 +0200

    Mise en com pour le moment

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+/* import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import ErrorPage from './pages/ErrorPage';
 import Appartment from './pages/AppartmentPage';
 import About from './pages/AboutPage';
+import Error from './pages/ErrorPage';
 
 const Routes = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/error404" component={ErrorPage} />
-        <Route path="/appartment" component={Appartment} />
-      </Switch>
+      <Route>
+        <Route exact path="/" element={Home} />
+        <Route path="/about" element={About} />
+        <Route path="/appartment" element={Appartment} />
+        <Route path="*" element={<Error />} />
+      </Route>
     </Router>
   );
 };
 
-export default Routes;
+export default Routes; */
