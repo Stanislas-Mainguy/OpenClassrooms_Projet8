732d3282e3d88e145dd5934e64f7b1679351fb4e
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-10 17:02:44 +0200

    Changement pour routage fix prob

--- src/index.jsx
+++ src/index.jsx
@@ -1,47 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes, useParams, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/main.scss';
 import Home from './pages/Home.jsx';
 import About from './pages/AboutPage';
 import Error from './pages/ErrorPage';
-import AppartmentPage from './pages/AppartmentPage';
+import Appartment from './pages/AppartmentPage';
 
-import data from './data/data.json';
 
 const root = createRoot(document.getElementById('root'));
-
-// ... Votre code précédent ...
 
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/appartment/:id" element={<Appartment />} />
         <Route path="/about" element={<About />} />
-        <Route path="/appartment/:id" element={<GetAppartmentPage />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   </React.StrictMode>
 );
-
-function GetAppartmentPage() {
-  const { id } = useParams();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const apartment = data.find(apartment => apartment.id === parseInt(id));
-
-    if (!apartment) {
-      navigate('/error404');
-    }
-  }, [id, navigate]);
-
-  return (
-    <div>
-      <AppartmentPage id={id} />
-    </div>
-  );
-}
