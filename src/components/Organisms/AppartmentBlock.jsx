e9b1d4e10a5676db4dc504963ed33af6f2369b4e
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-15 17:44:04 +0200

    Structure

--- src/components/Organisms/AppartmentBlock.jsx
+++ src/components/Organisms/AppartmentBlock.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Carousel from "../Molecules/Carousel";
+import HostInfo from "../Molecules/HostInfo";
+import AppartementInfo from "../Molecules/AppartmentInfo";
+import Collapse from "../"
 
 const AppartmentBlock = ({ appartement }) => {
     return (
-        <>
-            <h1>Appartment avec l'id : {appartement.id}</h1>
-        </>
+        <div className="appartment-block">
+            <Carousel appartement={appartement} />
+            <div className="appart-host-block">
+                <AppartementInfo appartement={appartement} />
+                <HostInfo appartement={appartement} />
+            </div>
+            <div className="collapse-block">
+                <Collapse appartement={appartement} page="Appartment" />
+            </div>
+        </div>
     );
 };
 
