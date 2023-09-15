19be88b8fa96346afa72172daa15c53f81f8e791
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-15 17:43:32 +0200

    Ajout class et changement en p

--- src/components/Molecules/Error.jsx
+++ src/components/Molecules/Error.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Error404 = () => {
   return (
     <div className="error">
-      <h1>404</h1>
-      <h2>Oups! La page que vous demandez n'existe pas.</h2>
+      <p className="error-404">404</p>
+      <p className="error-text">Oups! La page que<br className="error-return"/> vous demandez n'existe pas.</p>
       <p className="error-link">
         <Link to="/">Retourner sur la page d'accueil</Link>
       </p>
