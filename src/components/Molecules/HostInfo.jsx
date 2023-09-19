7939293ffc72c40b2d661c62ac0a1bbf28efe757
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-19 19:12:59 +0200

    Changement nom

--- src/components/Molecules/HostInfo.jsx
+++ src/components/Molecules/HostInfo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Rate from "../Atoms/Rate";
+import Stars from "./Stars";
 import HostPictureAndName from "../Atoms/HostPictureAndName";
 
 const HostInfo = ({ appartement }) => {
     return (
         <div className="host-block">
             <HostPictureAndName appartement={appartement} />
-            <Rate appartement={appartement} />
+            <Stars appartement={appartement} />
         </div>
     );
 };
