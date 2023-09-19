60dab7517601c495ff4a162374027239c48d03da
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-19 12:33:35 +0200

    Changement structure

--- src/components/Atoms/Rate.jsx
+++ src/components/Atoms/Rate.jsx
@@ -3,16 +3,11 @@ import ratePicture from "../../assets/pictures/rate.svg";
 
 const Rate = ({ appartement }) => {
     const rateNumber = appartement.rating;
-
-    // Définir la couleur de base des étoiles
     const baseStarColor = "#E3E3E3";
-
-    // Définir la couleur des étoiles en fonction du rating
     const starColor = rateNumber >= 1 ? "#FF6060" : baseStarColor;
 
     return (
         <div className="host-rating">
-            {/* Utiliser le SVG en tant qu'image et appliquer la couleur */}
             <img src={ratePicture} alt="Host Rating" style={{ fill: starColor }} />
         </div>
     );
