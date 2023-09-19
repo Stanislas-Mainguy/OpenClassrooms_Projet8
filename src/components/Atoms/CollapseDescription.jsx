df4a747344e3027e0c9040a8a69756f2e338de03
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-19 19:12:31 +0200

    Création Atoms

---
+++ src/components/Atoms/CollapseDescription.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CollapseDescript = ({ data, liste }) => {
+    if (liste) {
+      return (<ul>
+          {data.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>);
+    } else {
+      return <p>{data}</p>
+    };
+};
+
+export default CollapseDescript;
