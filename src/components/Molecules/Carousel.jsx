c7ec76f4acc36662e787b4f1291b7be5608aae71
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-19 12:34:10 +0200

    Changement pour display none si 1 photo

--- src/components/Molecules/Carousel.jsx
+++ src/components/Molecules/Carousel.jsx
@@ -7,38 +7,38 @@ const AppartmentCarousel = ({ appartement }) => {
   
     const handlePreviousClick = () => {
         if (currentImageIndex === 0) {
-        setCurrentImageIndex(appartement.pictures.length - 1);
+            setCurrentImageIndex(appartement.pictures.length - 1);
         } else {
-        setCurrentImageIndex(currentImageIndex - 1);
+            setCurrentImageIndex(currentImageIndex - 1);
         }
     };
 
     const handleNextClick = () => {
         if (currentImageIndex === appartement.pictures.length - 1) {
-        setCurrentImageIndex(0);
+            setCurrentImageIndex(0);
         } else {
-        setCurrentImageIndex(currentImageIndex + 1);
+            setCurrentImageIndex(currentImageIndex + 1);
         }
     };
+
+    const isSinglePhoto = appartement.pictures.length === 1;
 
     return (
         <div className="carousel">
             <div className="carousel-pictures">
                 <img src={appartement.pictures[currentImageIndex]} alt={`Visuel de l'appartement ${currentImageIndex + 1}`} />
             </div>
-            <div className="carousel-prev" onClick={handlePreviousClick}>
-                <img
-                    src={ArrowLeft}
-                    alt="Flèche gauche"
-                />
-            </div>
-            <span className="carousel-counter">{currentImageIndex + 1}/{appartement.pictures.length}</span>
-            <div className="carousel-next" onClick={handleNextClick}>
-                <img
-                    src={ArrowRight}
-                    alt="Flèche droite"
-                />
-            </div>
+            {!isSinglePhoto && (
+                <>
+                    <div className="carousel-prev" onClick={handlePreviousClick}>
+                        <img src={ArrowLeft} alt="Flèche gauche" />
+                    </div>
+                    <span className="carousel-counter">{currentImageIndex + 1}/{appartement.pictures.length}</span>
+                    <div className="carousel-next" onClick={handleNextClick}>
+                        <img src={ArrowRight} alt="Flèche droite" />
+                    </div>
+                </>
+            )}
         </div>
     );
 };
