d500156e4d8e0e7104619abfd59848a68a026040
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-09-20 17:17:49 +0200

    mapping css

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
+// Code pour le routage général de l'application //
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -5,7 +7,7 @@ import './styles/main.scss';
 import Home from './pages/Home.jsx';
 import About from './pages/AboutPage';
 import Error from './pages/ErrorPage';
-import Appartment from './pages/AppartmentPage';
+import Apartment from './pages/ApartmentPage';
 
 const root = createRoot(document.getElementById('root'));
 
@@ -14,7 +16,7 @@ root.render(
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/appartment/:id" element={<Appartment />} />
+        <Route path="/apartment/:id" element={<Apartment />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Error />} />
       </Routes>
